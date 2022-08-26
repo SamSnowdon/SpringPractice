5eb47ec38dde07191112517ece933bcccd6ef922
SamSnowdon <snowdonsam13@gmail.com>
2022-08-26 09:07:43 -0400

    Final Commit

--- src/main/java/com/example/springpractice/Repositories/BookRepository.java
+++ src/main/java/com/example/springpractice/Repositories/BookRepository.java
@@ -4,7 +4,11 @@ import com.example.springpractice.Entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Collection;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
 
 }
